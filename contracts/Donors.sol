0ea725ad1acc67434033ec08bb51ea1b755ca32c
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-09-21 11:49:12 +0300

    donor specific funcs

--- contracts/Donors.sol
+++ contracts/Donors.sol
@@ -14,9 +14,30 @@ contract Donors is Contributions{
   }
 
   //@dev function to check how much specific address has donated
-  function getAddressDonations(address addr) public view returns(uint){
+  function getAddressTotalDonations(address addr) public view returns(uint){
+    require(donorContributions[addr] != 0);
     return donorContributions[addr];
   }
+
+  //@dev function to check if address donated to specific missions
+  function isMissionDonor(address addr, string org) public view returns(uint8){
+    Mission storage mission = missions[org];
+    if(mission.contributors[addr] == 1){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+
+  //@dev get donor's contributions to specific mission
+  function getAddressToMissionDonations(address addr, string org) public view returns(uint){
+    Mission storage mission = missions[org];
+    require(mission.addressDonations[addr] != 0);
+    return mission.addressDonations[addr];
+  }
+
+
 
 
 }
