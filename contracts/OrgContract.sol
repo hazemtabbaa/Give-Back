8f0eb5b6d3e348e547f93d785edcbc94911294a5
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-10-30 20:38:35 -0700

    orgHead owner and recordPurchase function

--- contracts/OrgContract.sol
+++ contracts/OrgContract.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "./SafeMath.sol";
+import "./Charity.sol";
 
  //@dev contract used to give organization an address
  //this helps with monitoring the organization's work which helps
@@ -8,8 +9,30 @@ import "./SafeMath.sol";
  contract OrgContract{
    using SafeMath for uint;
    string organization;
+   address private orgOwner;
+   string [] purchases;
 
-   constructor(string _org) public{
+   modifier isOrgOwner{
+     require(msg.sender == orgOwner);
+     _;
+   }
+
+   constructor(string _org, address _orgHead) public{
      organization = _org;
+     orgOwner = _orgHead;
+   }
+
+   function getOrgBalance()
+    view public returns(uint){
+     return address(this).balance;
+   }
+
+   //@dev allow org to record purchases without changing balance (FOR NOW)
+   //due to assumption that they won't necessarily be in ETH
+   //fix later when even org purchases are in ETH
+   function recordPurchase(string _purchase)
+    isOrgOwner public returns(bool){
+     purchases.push(_purchase);
+     return true;
    }
  }
