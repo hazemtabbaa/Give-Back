93a2374d12e2f96fa7b5d49225eb94ddf48c63a3
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-11-03 20:04:34 -0700

    deploying all except orgContract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 var Charity = artifacts.require("./Charity.sol");
 var Donors = artifacts.require("./Donors.sol");
+var Contributions = artifacts.require("./Contributions.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
+    deployer.deploy(SafeMath);
+    deployer.link(SafeMath, Charity);
     deployer.deploy(Charity);
-    deployer.deploy(Charity);
+    //deployer.deploy(Charity);
+    deployer.deploy(Contributions);
+    deployer.deploy(Donors);
 };
