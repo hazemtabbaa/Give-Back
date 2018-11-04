7d977a46f3f59200907b6e6cb24f0520cc1b31a4
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-11-03 21:36:31 -0700

    request and approve

--- test/testCharity.js
+++ test/testCharity.js
@@ -1,4 +1,5 @@
 var Charity = artifacts.require('./Charity.sol');
+var Contributions = artifacts.require('./Contributions.sol');
 
 contract('Charity', function(accounts) {
   var owner = accounts[0];
@@ -6,30 +7,43 @@ contract('Charity', function(accounts) {
   var secondOrg = accounts[2];
   var firstDonor = accounts[3];
   var secondDonor = accounts[4];
+  var requester = accounts[5];
 
-  //var charity;
+  var charity;
 
-  /*beforeEach('setup contract for each test', async function () {
-          charity = await Charity.new();
+  beforeEach('setup contract for each test', async function () {
+          charity = await Charity.deployed();
   });
 
-  it('should have a valid owner', async function() {
+  /*it('should have a valid owner', async function() {
     assert.equal(charity.owner(), owner);
-  });
-
-  it('should create new mission', async function(){
-    var first = 'first';
-    await charity.createMission(first, 10, firstOrg);
-    assert.equal(1,1);
   });*/
 
-  it('should create new mission', async function(){
-    let charity = await Charity.deployed();
+  //Testing for correct number of missions after creating a new one
+  it('should create one new mission', async function(){
+    //charity = await Charity.deployed();
     await charity.createMission('first', 10,firstOrg);
     let missionCount = await charity.missionCounter.call();
-    console.log(charity.missionCounter());
-    assert.equal(missionCount, 1);
-  })
+    await console.log(charity.missionCounter());
+    missionCount = missionCount.toNumber();
+    await assert.equal(missionCount, 1);
+  });
+
+  //CONTRIBUTIONS TESTING
+  contract('Contributions', function(){
+
+    it('should allow user to request mission then owner to accept', async function(){
+      //charity = await Charity.deployed();
+      let contributions = await Contributions.deployed();
+      await contributions.requestMission.call('requestingMission');
+      await contributions.approveRequest.call('requestingMission');
+      let missionCount = await charity.missionCounter.call();
+      missionCount = missionCount.toNumber();
+      //await console.log(1234);
+      //await console.log(missionCount);
+      await assert.equal(missionCount, 1);
+    });
+  });
 
   it('should pass this test', function(){
     assert.equal(1,1);
