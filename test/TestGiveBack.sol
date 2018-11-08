4dc830a7e1ffca98a91213c45fa8489e580242e9
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-11-07 21:30:00 -0800

    commented

---
+++ test/TestGiveBack.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Charity.sol";
+import "../contracts/Contributions.sol";
+import "../contracts/Donors.sol";
+
+contract TestGiveBack{
+  /*function testCreateMission() {
+    //Charity charity = Charity(DeployedAddresses.Charity());
+    //charity.createMission('first', 10, tx.origin);
+    uint expected = 1;
+    //Assert.equal(1,1,"");
+    Assert.equal(uint(1), uint(1), "correct");
+    //Assert.equal(charity.missionCounter), expected, "Charity should have one mission");
+  }
+  function testTEST() public{
+    Assert.equal(uint(1), uint(1), "correct");
+  }*/
+
+}
