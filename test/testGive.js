490a3b382b53d5d61246ec77ac847cb560b21de0
Hazem Tabbaa <h1tabbaa@ucsd.edu>
2018-11-11 01:27:01 -0800

    Passing All except donate

--- test/testGive.js
+++ test/testGive.js
@@ -66,27 +66,81 @@ contract('Charity', function(accounts) {
       assert.equal(missionCount.toNumber(), 1);
     });
 
-    it('should have a balance of 10', async function(){
-      //let balanceOne = await charity.getBalance.call(firstOrg);
-      //console.log(balanceOne);
-      await charity.createMission('first', 10, firstOrg);
+    it('should return mission exists', async function(){
+      await contributions.createMission('first', 10, firstOrg);
+      let res = await contributions.missionExists('first');
+      //let res2 = await contributions.missionExists('fir');
+      res = res.toNumber();
+      //res2 = res2.toNumber();
+      //console.log('res 1 = ', res);
+      //console.log('res 2 = ', res2);
+      assert.equal(res,1);
+    });
+
+    it('should verify that mission does not exist', async function(){
+      await contributions.createMission('first', 10, firstOrg);
+      let res = await contributions.missionExists('test');
+      assert.equal(res,0);
+    });
+
+    it('firstOrg should receive 2', async function(){
+      await contributions.createMission('first', 10, firstOrg);
       //console.log("addr is ", firstOrg);
-      let orgBalance = await contributions.getMissionBalance.call('first');
-      console.log("initial balance, " , orgBalance.toNumber());
+      let orgBalance = await contributions.getMissionBalance('first');
+      orgBalance = orgBalance.toNumber();
+      console.log("initial balance = " , orgBalance);
+
+      let orgaddress = await contributions.getMissionAddress('first');
+      console.log('orgaddress is ', orgaddress);
+
+      //const initialBalance = web3.eth.getBalance(firstOrg).toNumber();
+
+      //let donorBalance = web3.eth.getBalance(firstDonor).toNumber();
+      //console.log("donor balance initial = ", donorBalance);
+
+      let receipt; //used for gasCost calculcation
       try {
-      await contributions.donate('first',{
+      receipt = await contributions.donate('first',{
         from: firstDonor,
-        value: /*web3.toWei(2)*/2
+        //gas:0,
+        gasPrice: 0,
+        value: web3.toWei(2)
       })
-    } catch (error) {
-      let donateError = error
-      console.error(`donateError: ${donateError}`)
-    }
+      } catch (error) {
+        let donateError = error
+        console.error(`donateError: ${donateError}`)
+      }
 
-      let secondOrgBalance = await contributions.getMissionBalance.call('first');
-      let difference = secondOrgBalance.toNumber() - orgBalance.toNumber();
 
-      console.log("DIFF, " , difference);
+      let gasUsed = receipt.receipt.gasUsed;
+      const tx = await web3.eth.getTransaction(receipt.tx);
+      let gasPrice = tx.gasPrice;
+
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
+      console.log(`GasPrice: ${tx.gasPrice}`);
+
+      let totalGas = gasPrice.mul(gasUsed);
+      totalGas = totalGas.toNumber();
+      let totalSent = web3.toWei(2) - totalGas;
+      console.log("totalSent = ", totalSent);
+
+      //let donorBalanceIn = web3.eth.getBalance(firstDonor).toNumber();
+      //console.log("donor balance POST = ", donorBalanceIn);
+      //console.log("donor difference = ", donorBalance-donorBalanceIn);
+
+      let exists = await contributions.missionExists('first');
+      console.log("exists = ", exists.toNumber());
+
+      let secondOrgBalance = await contributions.getMissionBalance('first');
+      secondOrgBalance = secondOrgBalance.toNumber();
+      //orgBalance = orgBalance + totalGas;
+      console.log("secondOrgBalance = ", secondOrgBalance);
+      console.log("Difference = ", secondOrgBalance - orgBalance);
+
+      orgaddress = await contributions.getMissionAddress('first');
+      console.log('orgaddress is second time ', orgaddress);
+      //console.log(`Final: ${final.toString()}`);
+      //assert.equal(final.add(gasPrice.mul(gasUsed)).toString(), orgBalance.toString(), "Must be equal");
       assert.equal(secondOrgBalance, orgBalance);
     });
 
